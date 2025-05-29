1c0a8883fed9d844319c7b53be5122d63863e845
Aryan.Udayakumar <aryanbsu132@gmail.com>
2025-05-29 18:57:07 +0400

    Update sketch.js

--- WORK WITH IMAGES/POINTILLISM/sketch.js
+++ WORK WITH IMAGES/POINTILLISM/sketch.js
@@ -1,5 +1,4 @@
 let img;
-let imgCopy;
 
 function preload() {
   img = loadImage('https://plus.unsplash.com/premium_photo-1667099522743-6b233d408465?q=80&w=1470&auto=format&fit=crop');
@@ -8,20 +7,24 @@ function preload() {
 function setup() {
   createCanvas(img.width, img.height);
   background(255);
-  imgCopy = createImage(img.width, img.height);
-  img.copy(0, 0, img.width, img.height, 0, 0, img.width, img.height);
   img.loadPixels();
 }
 
 function draw() {
-  let x = floor(random(img.width));
-  let y = floor(random(img.height));
-  let i = (x + y * img.width) * 4;
-  let r = img.pixels[i];
-  let g = img.pixels[i + 1];
-  let b = img.pixels[i + 2];
+  for (let n = 0; n < 500; n++) {
+    let x = floor(random(img.width));
+    let y = floor(random(img.height));
+    let i = (x + y * img.width) * 4;
+    let r = img.pixels[i];
+    let g = img.pixels[i + 1];
+    let b = img.pixels[i + 2];
 
-  fill(r, g, b);
-  noStroke();
-  ellipse(x, y, 5, 5);
+    fill(r, g, b);
+    noStroke();
+    ellipse(x, y, 5, 5);
+  }
+
+  if (frameCount > 300) {
+    noLoop();
+  }
 }
